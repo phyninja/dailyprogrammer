2107e03f65a0802e330ede7770cc42f1307afaa1
phyninja <rabh0153@colorado.edu>
2015-10-30 08:15:15 -0600

    added first challenge - random string

---
+++ random_string.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+ const char consonantArray[21] = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'};
+ const char vowelArray[5] = {'a','e','i','o','u'};
+ string Str;
+ cout<<"Enter your string: ";
+ cin >> Str;
+ srand (time(NULL));
+ for (unsigned int i=0; i<Str.length(); i++)
+ {
+	 if (Str[i] == *"c")
+	 {
+		int randIndex = rand()%22;
+		Str[i] = consonantArray[randIndex];
+	 }
+     else if (Str[i]==*"v")
+     {
+		int randIndex = rand()%6;
+		Str[i] = vowelArray[randIndex];
+	 }
+ }
+cout<<Str;
+
+}
